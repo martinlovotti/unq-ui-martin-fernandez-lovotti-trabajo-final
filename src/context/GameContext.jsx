47bb20e405ab127c61e59d5554280de0bc0e08bc
martinFL <martin0022f@gmail.com>
2025-12-05 12:43:19 -0300

    context

---
+++ src/context/GameContext.jsx
@@ -0,0 +1,59 @@
+import React, { createContext, useState, useEffect } from "react";
+import api from "../services/api";
+
+export const GameContext = createContext();
+
+export const GameProvider = ({ children }) => {
+  const [difficulty, setDifficulty] = useState(null);
+  const [questions, setQuestions] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [correctCount, setCorrectCount] = useState(0);
+  const [finished, setFinished] = useState(false);
+
+  // cargar preguntas cuando se elige dificultad
+  useEffect(() => {
+    if (difficulty) {
+      api.getQuestions(difficulty).then(setQuestions);
+      setCurrentIndex(0);
+      setCorrectCount(0);
+      setFinished(false);
+    }
+  }, [difficulty]);
+
+const answerQuestion = async (questionId, option) => {
+  const result = await api.sendAnswer(questionId, option);
+  if (result.answer) {
+    setCorrectCount((prev) => prev + 1);
+  }
+
+  // esperar 1 segundo antes de avanzar
+  setTimeout(() => {
+    if (currentIndex + 1 < questions.length) {
+      setCurrentIndex((prev) => prev + 1);
+    } else {
+      setFinished(true);
+    }
+  }, 1650);
+
+  return result;
+};
+
+
+
+
+  return (
+    <GameContext.Provider
+      value={{
+        difficulty,
+        setDifficulty,
+        questions,
+        currentIndex,
+        correctCount,
+        finished,
+        answerQuestion,
+      }}
+    >
+      {children}
+    </GameContext.Provider>
+  );
+};
